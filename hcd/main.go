4fd267bd83dc03ee4224d678892fbe110645a2e2
Alexander I.Grafov <grafov@inet.name>
2024-03-18 00:27:14 +0300

    Handle monitors disconnects
    Don't take into account yet that monitor index in ddcutil may be
    changed after monitors off/on.

--- hcd/main.go
+++ hcd/main.go
@@ -3,59 +3,20 @@ package main
 import (
 	"fmt"
 	"hcd/sensor"
-	"math"
-	"os/exec"
-	"strconv"
+	"hcd/tuner"
 	"time"
 )
 
 func main() {
 	sensor.Open()
-	var prev float64
-	update := time.NewTicker(5 * time.Second)
+	tuner.Setup()
+	update := time.NewTicker(500 * time.Millisecond)
 	for {
 		val := sensor.Read()
-		if val == 0 || math.Abs(val-prev) < 1 {
-			prev = val
-			continue
-		}
-		prev = val
 		select {
 		case <-update.C:
-			fmt.Printf("set brightness to %3.3f (raw)\n", val)
-			for i := 1; i <= 3; i++ {
-				m, max := tuneForMonitor(i)
-				br := int(val * m / 280 * max)
-				if br > int(max) {
-					br = int(max)
-				}
-				fmt.Printf("mon %d val %d \n", i, br)
-				if err := ddiset(i, br); err != nil {
-					fmt.Printf("error changing brightness on mon %d (%d): %s\n", i, br, err)
-				}
-				time.Sleep(time.Millisecond * 20)
-			}
+			fmt.Printf("brightness is %3.3f (raw)\n", val)
+			tuner.Set(val)
 		}
 	}
-}
-
-// another hack
-func tuneForMonitor(mon int) (mult, max float64) {
-	switch mon {
-	case 1: // NEC 24"
-		return 1.5, 7000
-	case 2: // LG 38"
-		return 1, 100
-	case 3: // NEC 27"
-		return 1, 100
-	default:
-		return 1, 100
-	}
-}
-
-// dirty hack based on ddcutil output
-func ddiset(mon int, b int) error {
-	cmd := exec.Command("/usr/bin/ddcutil", "-d", strconv.Itoa(mon), "setvcp", "10", strconv.Itoa(b))
-	_, err := cmd.Output()
-	return err
 }
---
+++ hcd/tuner/tuner.go
@@ -0,0 +1,90 @@
+package tuner
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var (
+	mons []chan float64
+	m    sync.Mutex
+)
+
+func Setup() {
+	for i := range 3 {
+		mon := make(chan float64, 60)
+		mons = append(mons, mon)
+		go func(mon chan float64, i int) {
+			var br, count int
+			var prevBr int
+			mul, max := tuneForMonitor(i)
+			for {
+				select {
+				case val := <-mon:
+					// получаем все накопившиеся значения
+					br = int(val * mul / 280 * max)
+					if br > int(max) {
+						br = int(max)
+					}
+					if prevBr == br {
+						continue
+					}
+					prevBr = br
+					count = 0 // получение нового значения останавливает ретраи старого
+				default:
+					// когда нет свежих значений в канале, обновляем яркость
+					switch {
+					case count > 50:
+						count = 0
+						br = 0
+					case br != 0:
+						if err := ddiset(i, br); err != nil {
+							fmt.Fprintf(os.Stderr, "error on mon %d (%d): %s\n", i, br, err)
+							count++
+							time.Sleep(time.Duration(count) * time.Second)
+						} else {
+							br = 0
+							count = 0
+						}
+					}
+					time.Sleep(3 * time.Second) // установка яркости не чаще этого периода
+				}
+			}
+		}(mon, i+1)
+	}
+}
+
+func Set(val float64) {
+	for _, m := range mons {
+		m <- val
+	}
+}
+
+// dirty hack based on ddcutil output
+func ddiset(mon int, b int) error {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	m.Lock() // для стабильности вызывать ddcutil последовательно для мониторов
+	cmd := exec.CommandContext(ctx, "/usr/bin/ddcutil", "-d", strconv.Itoa(mon), "setvcp", "10", strconv.Itoa(b))
+	_, err := cmd.Output()
+	m.Unlock()
+	return err
+}
+
+// another hack
+func tuneForMonitor(mon int) (mult, max float64) {
+	switch mon {
+	case 1: // NEC 24"
+		return 1.5, 7000
+	case 2: // LG 38"
+		return 1, 100
+	case 3: // NEC 27"
+		return 1, 100
+	default:
+		return 1, 100
+	}
+}
